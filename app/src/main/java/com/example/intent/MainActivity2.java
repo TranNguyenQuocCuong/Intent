e06700c34810df4e3d3588baedd8ab37a6eb215e
TranNguyenQuocCuong <cuongtnqde170165@fpt.edu.vn>
2024-05-21 16:45:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intent/MainActivity2.java
@@ -0,0 +1,47 @@
+package com.example.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    public TextView tvShow;
+    public Button btnReturn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main
+        ), (v, insets) -> {
+            Insets systemBars =
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top,
+                    systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tvShow = (TextView) findViewById(R.id.tvShow);
+        btnReturn = (Button) findViewById(R.id.buttonReturn);
+        if (getIntent() != null) {
+            String data = getIntent().getStringExtra("MESSAGE");
+            tvShow.setText("Hello " + data);
+        }
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
